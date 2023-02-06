76e3ca4492ba3fa05fd141537916fd8046ff3610
Daniel <danielgonsales93@gmail.com>
2023-02-06 11:28:28 +0300

    Initial Commit

---
+++ finalTrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalTrafficLight
+//
+//  Created by Daniel on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
